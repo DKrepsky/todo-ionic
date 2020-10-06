450675bded3986216e950c97cd9dca9ef9718d2f
David Krepsky <d.krepsky@outlook.com>
2020-10-06 07:10:06 -0300

    Resolução da atividade 2

--- src/app/pages/home/home.page.ts
+++ src/app/pages/home/home.page.ts
@@ -1,4 +1,6 @@
 import { Component } from '@angular/core';
+import { ToastController } from '@ionic/angular';
+import { TodoItem } from 'src/app/models/interfaces/todo-item.interface';
 
 @Component({
   selector: 'app-home',
@@ -6,7 +8,44 @@ import { Component } from '@angular/core';
   styleUrls: ['home.page.scss'],
 })
 export class HomePage {
+  public todos: Array<TodoItem> = [];
+  public newTodoName: string = "";
 
-  constructor() { }
+  constructor(
+    private toastCtrl: ToastController
+  ) { }
 
+  addTodo() {
+    if (this.newTodoName.trim().length == 0) {
+      this.notify("O nome da tarefa não pode ser vazio.");
+    } else if (this.newTodoName.trim().length > 64) {
+      this.notify(" O nome da tarefa não pode possuir mais que 64 caracteres.");
+    } else {
+      this.todos.push({
+        name: this.newTodoName,
+        done: false
+      });
+
+      this.newTodoName = "";
+    }
+  }
+
+  removeTodo(index: number) {
+    this.todos.splice(index, 1);
+  }
+
+  async notify(message: string) {
+    const toast = await this.toastCtrl.create({
+      message: message,
+      buttons: [
+        {
+          role: 'ok',
+          text: "OK"
+        }
+      ],
+      duration: 3000,
+    });
+
+    toast.present();
+  }
 }
