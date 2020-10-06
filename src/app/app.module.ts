b91216c14b3b056e744bda7f57576fdf7be0dde8
danielcollione <daniel.collione22@gmail.com>
2020-10-06 13:31:20 -0300

    Commit 5
    Importar em NgModule o componente ToDoItem criado.

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,12 +3,13 @@ import { BrowserModule } from '@angular/platform-browser';
 import { RouteReuseStrategy } from '@angular/router';
 
 import { IonicModule, IonicRouteStrategy } from '@ionic/angular';
+import {TodoItemComponent} from './components/todo-item/todo-item.component';
 
 import { AppComponent } from './app.component';
 import { AppRoutingModule } from './app-routing.module';
 
 @NgModule({
-  declarations: [AppComponent],
+  declarations: [AppComponent, TodoItemComponent],
   entryComponents: [],
   imports: [BrowserModule, IonicModule.forRoot(), AppRoutingModule],
   providers: [
