e440bd014157bebcbcc7cdfce7b35c6cb3790463
danielcollione <daniel.collione22@gmail.com>
2020-10-08 20:28:10 -0300

    Commit 2
    Declarando as diretivas do rxjs Observable, BeahaviorSubject, Subject no todo.services. Necessário criação de aux para declara o BehaviorSubject e criação do método getTodo()

--- src/app/components/todo-item/todo-item.component.ts
+++ src/app/components/todo-item/todo-item.component.ts
@@ -1,5 +1,7 @@
 import { Component, EventEmitter, Input, OnInit, Output } from '@angular/core';
 import { ToastController } from '@ionic/angular';
+import { ToDoItemList } from 'src/app/models/types/todo-list.type';
+import {TodoService} from '../../services/todo/todo.service';
 
 @Component({
   selector: 'app-todo-item',
@@ -7,6 +9,8 @@ import { ToastController } from '@ionic/angular';
   styleUrls: ['./todo-item.component.scss'],
 })
 export class TodoItemComponent implements OnInit {
+
+  todosTest: ToDoItemList;
 
   @Input() todos: Array<any>;
   @Input() todoTitle: string;
@@ -22,9 +26,23 @@ export class TodoItemComponent implements OnInit {
   @Input() SuccessToast: (success) => void;
 
 
-  constructor(public toastController: ToastController) { }
+  constructor(public toastController: ToastController, private todoService: TodoService) { 
+    this.getter();
+  }
 
   ngOnInit() {}
+
+  getter(){
+    this.todoService.getTodo().subscribe(
+      data => { 
+        this.todosTest = [data];
+        console.log(this.todosTest);
+        console.log(data);
+      }, 
+      error => {
+        console.log('deu ruim');
+      })
+  }
 
   
 
--- src/app/services/todo/todo.service.ts
+++ src/app/services/todo/todo.service.ts
@@ -1,15 +1,39 @@
 import { Injectable } from '@angular/core';
-import { Observable } from 'rxjs';
-import { ToDoItemList } from 'src/app/models/types/todo-list.type';
+import { ToDoItem } from 'src/app/models/interfaces/todo-item.interface';
+import { BehaviorSubject } from 'rxjs';
+import { Observable } from 'rx';
 
 @Injectable({
   providedIn: 'root'
 })
 export class TodoService {
+  private _todos: BehaviorSubject<Array<ToDoItem>>
 
   constructor() { }
 
-  getTodo(): Observable<ToDoItemList> {
+  public getTodo(): Observable<ToDoItem> {
+    
+    const data = this._todos.next([
+      {
+        'id': 1,
+        'title': 'Conhecer o Ionic',
+        'completed': true,
+      },
+      {
+        'id': 2,
+        'title': 'Conhecer o Angular',
+        'completed': false,
+      },
+      {
+        'id': 3,
+        'title': 'Concluir as atividades',
+        'completed': false,
+      },
+    ])
+    console.log(this._todos);
 
+    return data;
+    
+    
   }
 }
